d588120bf170e879b4fbc77c1e4acbb791145d32
Brianoyaro <brianoyaro2000@yahoo.com>
2023-11-18 13:43:02 +0300

    quick-sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,67 +1,45 @@
-#include <stdio.h>
 #include "sort.h"
-/**
- * quick_sort - uses lomuto partion scheme sorter
- * @array: array to sort
- * @size: length of array
- */
-void quick_sort(int *array, size_t size)
+void swap(int* one, int* two) 
 {
-	size_t low, high;
+	int temp;
 
-	low = 0;
-	high = size - 1;
-	lomuto(array, low, high);
-}
-/**
- * lomuto - partitions an array
- * @array: array to partition
- * @low: lowest index of array
- * @high: highest index of array
- * Return: last swapped index
- */
-int partition(int *array, size_t low, size_t high)
+	temp = *one;
+	*one = *two;
+	*two = temp;
+} 
+
+int partition(int *array, int start, int end, size_t size)
 {
-	int pivot = 0, temp = 0;
-	size_t i = 0, j = 0;
-
-	pivot = array[high];
-	/*lowest index so far*/
-	i = low;
-	for (j = low; j < high; j++)
+	int pivotVal, i, j;
+	
+	pivotVal = array[end];
+	i = start;
+	for (j = start; j < end; j++)
 	{
-		if (array[j] < pivot)
+		if (array[j] <= pivotVal)
 		{
-			printf("\narray[j]: %d < pivot: %d\n", array[j], pivot);
-			temp = array[j];
-			array[j] = array[i];
-			array[i] = temp;
-			++i;
-			print_array(array, high);
+			swap(&array[i], &array[j]);
+			/*print_array(array, size);*/
+			i++;
 		}
 	}
-	/*swap highest and lowest*/
-	printf("\narray[i]: %d swap with  array[high]: %d\n", array[i], array[high]);
-	temp = array[i];
-	array[i] = array[high];
-	array[high] = temp;
-	print_array(array, high);
+	swap(&array[i], &array[end]);
+	print_array(array, size);
 	return (i);
 }
-/**
- * lomuto - quicksorts an array
- * @array: array to quicksort
- * @low: lowest index of array
- * @high: highest index of array
- */
-void lomuto(int *array, size_t low, size_t high)
-{
-	int val;
 
-	if (low < high)
+void quickSort(int *array, int start, int end, size_t size)
+{
+	int pivotval;
+
+	if (start < end)
 	{
-		val = partition(array, low, high);
-		/*lomuto(array, low, val - 1);*/
-		lomuto(array, val + 1, high);
+		pivotval = partition(array, start, end, size);
+		quickSort(array, start, pivotval - 1, size);
+		quickSort(array, pivotval + 1, end, size);
 	}
+}
+void quick_sort(int *array, size_t size)
+{
+	quickSort(array, 0, size - 1, size);
 }
